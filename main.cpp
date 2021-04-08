c662ad6b83a684f441c1b38c1cdac84a773bfde9
Asish Upadhyay <>
2021-04-08 20:40:50 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+using namespace std;
+
+class tictactoe{
+	string player1,player2;
+	char a[3][3];
+	public:
+	tictactoe(string p1,string p2)
+	{
+		player1=p1;
+		player2=p2;
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+				a[i][j]='-';
+		}
+	}
+	
+	
+	void display(){
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+				cout<<a[i][j]<<" ";
+			cout<<endl;
+		}
+		
+	}
+	
+	string getPlayer1()
+	{
+		return player1;
+	}
+	
+	string getPlayer2()
+	{
+		return player2;
+	}
+    
+	int winner(int x,int y)
+	{
+		if(x==0&&y==0)
+		{
+			if(a[x+1][y]==a[x][y]&&a[x+2][y]==a[x][y])
+				return 1;
+			if(a[x][y+1]==a[x][y]&&a[x][y+2]==a[x][y])
+				return 1;
+			if(a[x+1][y+1]==a[x][y]&&a[x+2][y+2]==a[x][y])
+				return 1;
+		}
+		if(x==0&&y==1)
+		{
+			if(a[x+1][y]==a[x][y]&&a[x+2][y]==a[x][y])
+				return 1;
+			if(a[x][y+1]==a[x][y]&&a[x][y-1]==a[x][y])
+				return 1;
+		}
+		if(x==0&&y==2)
+		{
+			if(a[x+1][y]==a[x][y]&&a[x+2][y]==a[x][y])
+				return 1;
+			if(a[x][y-1]==a[x][y]&&a[x][y-2]==a[x][y])
+				return 1;
+			if(a[x+1][y-1]==a[x][y]&&a[x+2][y-2]==a[x][y])
+				return 1;
+		}
+		if(x==1&&y==0)
+		{
+			if(a[x+1][y]==a[x][y]&&a[x-1][y]==a[x][y])
+				return 1;
+			if(a[x][y+1]==a[x][y]&&a[x][y+2]==a[x][y])
+				return 1;
+		}
+		if(x==1&&y==1)
+		{
+			if(a[x+1][y]==a[x][y]&&a[x-1][y]==a[x][y])
+				return 1;
+			if(a[x][y-1]==a[x][y]&&a[x][y+1]==a[x][y])
+				return 1;
+			if(a[x-1][y-1]==a[x][y]&&a[x+1][y+1]==a[x][y])
+				return 1;
+		}
+		if(x==1&&y==2)
+		{
+			if(a[x+1][y]==a[x][y]&&a[x-1][y]==a[x][y])
+				return 1;
+			if(a[x][y-1]==a[x][y]&&a[x][y-2]==a[x][y])
+				return 1;
+		}
+		if(x==2&&y==0)
+		{
+			if(a[x-1][y]==a[x][y]&&a[x-2][y]==a[x][y])
+				return 1;
+			if(a[x][y+1]==a[x][y]&&a[x][y+2]==a[x][y])
+				return 1;
+			if(a[x-1][y+1]==a[x][y]&&a[x-2][y+2]==a[x][y])
+				return 1;
+			
+		}
+		if(x==2&&y==1)
+		{
+			if(a[x-1][y]==a[x][y]&&a[x-2][y]==a[x][y])
+				return 1;
+			if(a[x][y+1]==a[x][y]&&a[x][y-1]==a[x][y])
+				return 1;
+			
+		}
+		if(x==2&&y==2)
+		{
+			if(a[x-1][y]==a[x][y]&&a[x-2][y]==a[x][y])
+				return 1;
+			if(a[x][y+1]==a[x][y]&&a[x][y+2]==a[x][y])
+				return 1;
+			if(a[x-1][y-1]==a[x][y]&&a[x-2][y-2]==a[x][y])
+				return 1;
+			
+		}
+		
+		return 0;
+		
+	}
+	
+	int move(int x,int y,int moves)
+	{
+		if(x<0||y<0||x>2||y>2||a[x][y]!='-')
+			return -1;
+		if(moves%2==0)
+			a[x][y]='x';
+		else
+			a[x][y]='o';
+			
+		int result=winner(x,y);
+			
+		display();
+		return result;
+	}
+};
+
+int main() {
+	// your code goes here
+	string player1,player2;
+	cin>>player1>>player2;
+	tictactoe game(player1,player2);
+	game.display();
+	int moves=0;
+	int finished=0;
+	while(finished==0&&moves<9)
+	{
+ 		cout<<"Enter the x and y components\n";
+ 		int x,y;
+ 		cin>>x>>y;
+		if(!x &&!y)
+			moves=9;
+			
+ 		int result=0;
+ 		result=game.move(x,y,moves);
+ 		if(result==-1)
+ 			cout<<"Invalid Move\n";
+ 		else if(result==0)
+ 			moves++;
+ 		else{
+ 			finished=1;
+ 			if(moves%2==0)
+ 				cout<<game.getPlayer1()<<" is the winner\n";
+ 			else
+ 				cout<<game.getPlayer2()<<" is the winner\n";
+				
+		}
+		
+	}
+	
+	if(finished==0)
+		cout<<"It's a draw\n";
+	
+	return 0;
+}
